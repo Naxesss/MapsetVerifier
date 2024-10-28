5dea5a8a59d0b5cbaf5d064d6ed3718125d6195c
Naxess <30292137+Naxesss@users.noreply.github.com>
2024-10-28 10:27:30 +0100

    Add `pause` so the cmd logs can be reviewed

--- make-local-dist.bat
+++ make-local-dist.bat
@@ -2,3 +2,5 @@ dotnet build src -c Release -r win-x86 -o app/api/win-x86 --self-contained
 dotnet build src -c Release -r linux-x64 -o app/api/linux-x64 --self-contained
 
 call npm run dist
+
+pause
