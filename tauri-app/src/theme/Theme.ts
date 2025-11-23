882bedf096223e559e6f83026eda3b608b300332
Darius Wattimena <dariuswattimena@hotmail.com>
2025-11-24 00:14:56 +0100

    Add dark and primary colors to theme

--- tauri-app/src/theme/Theme.ts
+++ tauri-app/src/theme/Theme.ts
@@ -52,6 +52,32 @@ const orange: MantineColorsTuple = [
   "#af7100",
 ];
 
+const dark: MantineColorsTuple = [
+  "#c5cad3",
+  "#a3adbd",
+  "#8695ab",
+  "#424D61",
+  "#353F52",
+  "#283243",
+  "#212B39",
+  "#1E2734",
+  "#161D28",
+  "#0D121A",
+] as const;
+
+const primary: MantineColorsTuple = [
+  "#e1f8ff",
+  "#cbedff",
+  "#99ccff", // Main color
+  "#64c1ff",
+  "#3aaefe",
+  "#20a2fe",
+  "#099cff",
+  "#0088e4",
+  "#0079cd",
+  "#0068b6",
+] as const;
+
 const themeOverride = createTheme({
   defaultRadius: 5,
   spacing: {
@@ -66,7 +92,9 @@ const themeOverride = createTheme({
     red: red,
     green: green,
     orange: orange,
-  },
+    dark: dark,
+    primary: primary
+  }
 });
 
 export const theme = mergeMantineTheme(DEFAULT_THEME, themeOverride);
