251e099f381659dbd0aef7e8f0383e7000d1d2f1
Darius Wattimena <dariuswattimena@hotmail.com>
2025-11-26 19:29:18 +0100

    Add ESLint and Prettier

---
+++ tauri-app/.eslintrc.cjs
@@ -0,0 +1,78 @@
+﻿module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+    project: ['./tsconfig.json'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'import',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  rules: {
+    // TypeScript
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    // React
+    'react/react-in-jsx-scope': 'off', // React 17+
+    'react/jsx-uses-react': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+
+    // Imports
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+          'type',
+        ],
+        'newlines-between': 'never',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+
+    // General
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prefer-const': 'warn',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-undef': 'off', // TypeScript handles this
+      },
+    },
+  ],
+  ignorePatterns: [
+    'dist',
+    'node_modules',
+  ],
+};
---
+++ tauri-app/prettier.config.cjs
@@ -0,0 +1,10 @@
+﻿module.exports = {
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'always',
+};
+
