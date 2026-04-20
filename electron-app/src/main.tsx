09b336d5dbd96b648bed93c23fa86ca3601335c9
Hivie <me@hivie.tn>
2026-04-20 23:10:25 +0100

    disable strict mode

--- electron-app/src/main.tsx
+++ electron-app/src/main.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
@@ -8,10 +7,8 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <React.StrictMode>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
-    </React.StrictMode>
   </BrowserRouter>
 );
