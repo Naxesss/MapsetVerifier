de40aaa2912fa95516c76137f9fddb7045d50da8
Darius Wattimena <dariuswattimena@hotmail.com>
2026-05-10 20:31:48 +0200

    Fix linter complaining

--- electron-app/src/components/backend/BackendGate.tsx
+++ electron-app/src/components/backend/BackendGate.tsx
@@ -14,7 +14,6 @@
 } from '@mantine/core';
 import { IconAlertCircle, IconCheck, IconCopy, IconTerminal2 } from '@tabler/icons-react';
 import React, { useEffect, useState, useRef, useCallback, ReactNode } from 'react';
-
 import { cssVarResolver } from '../../App';
 import { BACKEND_BASE_URL } from '../../Constants.ts';
 import { useSettings } from '../../context/SettingsContext';
