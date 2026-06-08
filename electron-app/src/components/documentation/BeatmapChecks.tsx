1397dcac09b37d5fdb3789cd64440fdabb3e7fe9
Darius Wattimena <dariuswattimena@hotmail.com>
2026-06-08 19:12:10 +0200

    Updated beatmap checks documentation loading and error to be consistent

--- electron-app/src/components/documentation/BeatmapChecks.tsx
+++ electron-app/src/components/documentation/BeatmapChecks.tsx
@@ -1,8 +1,9 @@
-﻿import { Group, Text } from '@mantine/core';
+﻿import {Alert, Group, Loader, Text} from '@mantine/core';
 import DocumentationCheck from './DocumentationCheck';
 import { useBeatmapDocumentationChecks } from './hooks/useDocumentationChecks';
 import { Mode } from '../../Types.ts';
 import { formatGameModeLabel } from '../../utils/gameMode';
+import {IconAlertCircle} from "@tabler/icons-react";
 
 interface BeatmapChecksProps {
   mode: Mode;
@@ -11,8 +12,14 @@ interface BeatmapChecksProps {
 function BeatmapChecks({ mode }: BeatmapChecksProps) {
   const { checks, isLoading, isError } = useBeatmapDocumentationChecks(mode);
 
-  if (isLoading) return <Text>Loading...</Text>;
-  if (isError) return <Text>Error loading checks.</Text>;
+  if (isLoading) return <Loader size="sm" />;
+  if (isError) {
+    return (
+      <Alert icon={<IconAlertCircle />} color="red">
+        Failed to load {formatGameModeLabel(mode)} checks.
+      </Alert>
+    );
+  }
   if (!checks || checks.length === 0)
     return <Text>No {formatGameModeLabel(mode)} checks found.</Text>;
 
