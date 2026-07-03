a26f9e65c37892e2b1578c6d514ab618ebc159da
Darius Wattimena <dariuswattimena@hotmail.com>
2026-07-03 22:52:57 +0200

    Adjust behaviour of settings button to go back to the previous page when clicking again

--- electron-app/src/components/settings/SettingsButton.tsx
+++ electron-app/src/components/settings/SettingsButton.tsx
@@ -1,16 +1,29 @@
 import { NavLink } from '@mantine/core';
 import { IconSettings } from '@tabler/icons-react';
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const SettingsButton: React.FC = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const active = location.pathname.startsWith('/settings');
+  const previousPathRef = useRef<string>('/');
+
+  if (!active) {
+    previousPathRef.current = location.pathname + location.search;
+  }
+
+  const handleClick = () => {
+    if (active) {
+      navigate(previousPathRef.current);
+    } else {
+      navigate('/settings');
+    }
+  };
 
   return (
     <NavLink
-      component={Link}
-      to="/settings"
+      onClick={handleClick}
       viewTransition
       w={'unset'}
       label={<IconSettings />}
